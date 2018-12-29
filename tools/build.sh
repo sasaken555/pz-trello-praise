2e6cdbee73840896b11970d5ca8bef70f35cd2bc
Kentaro Sasaki <accelerk@gmail.com>
2018-12-30 02:00:52 +0900

    パラメータの置換処理でURLを含む場合は区切り文字を|にする

--- tools/build.sh
+++ tools/build.sh
@@ -5,13 +5,14 @@ echo ">>> Install dependencies..."
 npm install
 
 # Replace parameters with environment variables
+#区切り文字'/'だとURLの中で区切られてしまうため, URLを含む場合は区切り文字を'|'にする
 echo ">>> Replace parameters..."
 echo ">>> Replace Function parameters..."
-sed -i -e "s/@TRELLO_URL@/$TRELLO_URL/g" serverless.yml
+sed -i -e "s|@TRELLO_URL@|$TRELLO_URL|g" serverless.yml
 sed -i -e "s/@TRELLO_API_KEY@/$TRELLO_API_KEY/g" serverless.yml
 sed -i -e "s/@TRELLO_TOKEN@/$TRELLO_TOKEN/g" serverless.yml
 echo ">>> Replace Slack Package parameters..."
-sed -i -e "s/@SLACK_WEBHOOK_URL@/$SLACK_WEBHOOK_URL/g" serverless.yml
+sed -i -e "s|@SLACK_WEBHOOK_URL@|$SLACK_WEBHOOK_URL|g" serverless.yml
 sed -i -e "s/@SLACK_APP_USER@/$SLACK_APP_USER/g" serverless.yml
 sed -i -e "s/@SLACK_CHANNEL@/$SLACK_CHANNEL/g" serverless.yml
 
